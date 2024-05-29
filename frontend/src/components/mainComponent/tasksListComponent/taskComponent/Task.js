a86bbe4554bdc2d2342f464a8e72acceebe32da1
itsdokic <imrandokic03@gmail.com>
2024-05-29 19:10:00 +0200

    Drugi dan
    -Dodana opcija spremanja task-a u favorite
    -Dodana opcija editovanja task-a
    -Ljepši ispis taskova

--- frontend/src/components/mainComponent/tasksListComponent/taskComponent/Task.js
+++ frontend/src/components/mainComponent/tasksListComponent/taskComponent/Task.js
@@ -1,22 +1,72 @@
 import './Task.css';
+import React from 'react';
 import DeleteIcon from '@mui/icons-material/Delete';
 import IconButton from '@mui/material/IconButton';
+import StarOutlineIcon from '@mui/icons-material/StarOutline';
+import StarIcon from '@mui/icons-material/Star';
 import axios from 'axios';
+import EditTask from './editTaskComponent/EditTask';
 
 function Task(props) {
 
     async function completeTask(taskId) {
-        await axios.post("http://localhost:5000/completeTask", {
-            id: taskId
-          })
-          .then(
-            (response) => {
-              console.log("Uspješno")
-            },
-            (error) => {
-              console.log("Error", error)
-            }
-           );
+        if (!props.task.completed) {
+            await axios.post("http://localhost:5000/completeTask", {
+                id: taskId
+            })
+            .then(
+                (response) => {
+                  console.log("Uspješno")
+                },
+                (error) => {
+                  console.log("Error", error)
+                }
+            );
+        }
+        else {
+            await axios.post("http://localhost:5000/uncompleteTask", {
+                id: taskId
+            })
+            .then(
+                (response) => {
+                console.log("Uspješno")
+                },
+                (error) => {
+                console.log("Error", error)
+                }
+            );
+        }
+
+        props.getTasks();
+   }
+
+   async function favorTask(taskId) {
+        if (!props.task.favorite) {
+            await axios.post("http://localhost:5000/favorTask", {
+                id: taskId
+            })
+            .then(
+                (response) => {
+                console.log("Uspješno")
+                },
+                (error) => {
+                console.log("Error", error)
+                }
+            );
+        }
+        else {
+            await axios.post("http://localhost:5000/unfavorTask", {
+                id: taskId
+            })
+            .then(
+                (response) => {
+                console.log("Uspješno")
+                },
+                (error) => {
+                console.log("Error", error)
+                }
+            );
+        }
 
         props.getTasks();
    }
@@ -40,9 +90,15 @@ function Task(props) {
             <p  onClick={() => completeTask(props.task._id)} 
                 className={props.task.completed ? "completedTask" : "uncompletedTask"}  
                 id = {props.task._id}> {props.task.task} </p>
-            <IconButton onClick={() => removeTask(props.task._id)}>
-                <DeleteIcon /> 
-            </IconButton>
+            <div className="buttons">
+                <EditTask task={props.task.task} taskId={props.task._id} getTasks={() => props.getTasks()} /> 
+                <IconButton onClick={() => favorTask(props.task._id)}>
+                    {props.task.favorite ? <StarIcon /> : <StarOutlineIcon /> }
+                </IconButton>
+                <IconButton onClick={() => removeTask(props.task._id)}>
+                    <DeleteIcon /> 
+                </IconButton>
+            </div>
         </div>
     )
 }
--- server/routes/index.js
+++ server/routes/index.js
@@ -43,11 +43,72 @@ router.post("/completeTask", async (req, res) => {
 
 });
 
+router.post("/uncompleteTask", async (req, res) => {
+  const taskId = req.body.id;
+ 
+  try {
+    await TaskModel.findByIdAndUpdate(
+      taskId,
+      {completed: false}
+    );
+    res.send(taskId);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+
+});
+
+router.post("/editTask", async (req, res) => {
+  const taskId = req.body.taskId;
+  const editedTaskName = req.body.task;
+ 
+  try {
+    await TaskModel.findByIdAndUpdate(
+      taskId,
+      {task: editedTaskName}
+    );
+    res.send(taskId);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+
+});
+
+router.post("/favorTask", async (req, res) => {
+  const taskId = req.body.id;
+ 
+  try {
+    await TaskModel.findByIdAndUpdate(
+      taskId,
+      {favorite: true}
+    );
+    res.send(taskId);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+
+});
+
+router.post("/unfavorTask", async (req, res) => {
+  const taskId = req.body.id;
+ 
+  try {
+    await TaskModel.findByIdAndUpdate(
+      taskId,
+      {favorite: false}
+    );
+    res.send(taskId);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+
+});
+
 router.delete("/removeTask/:id", async (req, res) => {
  
   try {
     await TaskModel.findByIdAndDelete(req.params.id);
-    res.send(req.params.id);
+    res.send(req.params);
    } catch (error) {
      res.status(500).send({ error });
    }
